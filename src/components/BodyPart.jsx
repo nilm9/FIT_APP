e90f089fc19e54be1eb742ba84416dfd86270119
Nil Monfort <nil.monfort@estudiants.urv.cat>
2022-10-01 21:57:22 +0200

    Exercises tab

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -12,6 +12,9 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
     height: '280px',
     cursor: 'pointer',
     gap: '47px'
+    }} onClick={()=>{
+      setBodyPart(item);
+      window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
     }} >
       <img src={Icon} alt="dumbell" style={{width: '40px', height: '40px'}} />
       <Typography fontSize="24px" fontWeight="bold" color="#3A1212">
